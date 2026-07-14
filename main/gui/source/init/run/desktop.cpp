33b213d32d382b24f5654c7312d9c157229ebc2d
high on tantor <josephvenable@verizon.net>
2026-07-13 22:18:45 -0400

    Update desktop.cpp

--- main/gui/source/init/run/desktop.cpp
+++ main/gui/source/init/run/desktop.cpp
@@ -7,9 +7,9 @@
     #include <init/run.hpp>
     #include <window.hpp>
 
-    #if defined(OS_LINUX) && !defined(GLFW_EXPOSE_NATIVE_X11)
+#if defined(OS_LINUX) && !defined(GLFW_EXPOSE_NATIVE_X11)
     #define GLFW_EXPOSE_NATIVE_X11
-    #endif
+#endif
 
     #include <GLFW/glfw3.h>
     #include <GLFW/glfw3native.h>
